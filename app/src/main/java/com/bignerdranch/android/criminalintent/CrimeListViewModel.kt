4c20efdfab3b409534fb0e538a4a9f0c00a3926a
Colby Sawyer <sawyerc17@students.ecu.edu>
2020-06-04 12:32:29 -0400

    End of Chapter 14

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -6,5 +6,9 @@ class CrimeListViewModel : ViewModel() {
 
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
 }
 
